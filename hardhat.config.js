381a35194a699af72e32ea0c58169dca4ce45c63
chillivo <khoavo.sound@gmail.com>
2022-02-26 11:20:58 +0700

    update config deploy & verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,23 @@
-require("dotenv").config();
+// Loading env configs for deploying and public contract source
+require('dotenv').config();
 
-require("@nomiclabs/hardhat-etherscan");
+// Using hardhat-ethers plugin for deploying
+// See here: https://hardhat.org/plugins/nomiclabs-hardhat-ethers.html
+//           https://hardhat.org/guides/deploying.html
+require('@nomiclabs/hardhat-ethers');
+
+// Testing plugins with Waffle
+// See here: https://hardhat.org/guides/waffle-testing.html
 require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
+
+// This plugin runs solhint on the project's sources and prints the report
+// See here: https://hardhat.org/plugins/nomiclabs-hardhat-solhint.html
+require("@nomiclabs/hardhat-solhint");
+
+// Verify and public source code on etherscan
+require('@nomiclabs/hardhat-etherscan');
+
+// require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,23 +32,54 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
+const config = {
+  defaultNetwork: "hardhat",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
+      accounts: { count: 100 },
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    binance_testnet: {
+      url: 'https://data-seed-prebsc-1-s2.binance.org:8545/',
+      accounts: [process.env.DEPLOY_ACCOUNT],
+    },
+    rinkeby_testnet: {
+      url: "https://rinkeby.infura.io/v3/237c133d742644c79c0c50166d6c2d3f",
+      accounts: [process.env.DEPLOY_ACCOUNT]
+    }
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.RINKEBY_API_KEY,
+    // apiKey: process.env.BINANCE_API_KEY,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+    deploy: "deploy",
+    deployments: "deployments",
+  },
+  mocha: {
+    timeout: 200000,
+    useColors: true,
+    reporter: "mocha-multi-reporters",
+    reporterOptions: {
+      configFile: "./mocha-report.json",
+    },
   },
 };
+
+module.exports = config;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,38 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const fs = require("fs");
+const ethers = hre.ethers;
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  //Loading accounts
+  const accounts = await ethers.getSigners();
+  const addresses = accounts.map(item => item.address);
+  const admin = addresses[0];
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // Loading contract factory.
+  const Test = await ethers.getContractFactory("test");
 
-  await greeter.deployed();
+  // Deploy contracts
+  console.log("=======================================================");
+  console.log("DEPLOY CONTRACTS");
+  console.log("=======================================================");
 
-  console.log("Greeter deployed to:", greeter.address);
+  const test = await Test.deploy();
+  await test.deployed();
+  console.log("Test deployed to:", test.address);
+
+  const contractAddresses = {
+    admin: admin,
+    test: test.address
+  };
+
+  await fs.writeFileSync("contracts.json", JSON.stringify(contractAddresses));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
